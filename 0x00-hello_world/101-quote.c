72a84d1b2669dbbfe0f1e03fb16dc1c04ea39328
FavouredAda <igwefavoured@gmail.com>
2023-09-07 17:51:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints quote to the standard error
+ * main - Entry point
  *
- * Return: zero on success
- *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	rturn (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
