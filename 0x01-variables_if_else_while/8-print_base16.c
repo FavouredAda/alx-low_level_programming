95105fbb7493721b80da938809b34bec057429c3
FavouredAda <igwefavoured@gmail.com>
2023-09-09 01:09:11 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers in base 16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
