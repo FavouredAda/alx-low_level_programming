071c0ce3111ea52ea014f75a63b1496a5862aa8b
FavouredAda <igwefavoured@gmail.com>
2023-09-09 03:02:30 +0000

    print combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 100; j++)
+	for (i = 0; i <= 98; j++)
 	{
 		if (i < j)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
 			if (i != 98 || j != 99)
 			{
+				continue;
 				putchar(',');
 				putchar(' ');
 			}
