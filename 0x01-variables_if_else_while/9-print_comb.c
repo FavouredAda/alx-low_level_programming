dd4a0c6b2009a3510db6c8ab4caeb1b12123149f
FavouredAda <igwefavoured@gmail.com>
2023-09-09 01:17:47 +0000

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single digit number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
